19f3e908c6db0bb2994561253ac529c2cdab3c4b
Otavio Santana <otaviopolianasantana@gmail.com>
2026-04-28 20:24:55 +0100

    fix: use CardOperationStatus enum for card status in seeding logic

--- src/main/java/com/acme/cards/command/Card.java
+++ src/main/java/com/acme/cards/command/Card.java
@@ -9,6 +9,7 @@ import jakarta.nosql.Entity;
 import jakarta.nosql.Id;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -31,5 +32,27 @@ public class Card {
         this.cardId = cardId;
         this.availableBalance = availableBalance;
         this.cardOperationStatus = cardOperationStatus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Card card)) {
+            return false;
+        }
+        return Objects.equals(cardId, card.cardId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(cardId);
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "cardId=" + cardId +
+                ", availableBalance=" + availableBalance +
+                ", cardOperationStatus=" + cardOperationStatus +
+                '}';
     }
 }

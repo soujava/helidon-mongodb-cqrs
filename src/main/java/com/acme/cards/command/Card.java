fa5b21a8c56c2de3f579cebfcf096780927f86c1
Otavio Santana <otaviopolianasantana@gmail.com>
2026-04-28 20:10:04 +0100

    refactor: move TransactionView to com.acme.cards.query package for better structure

--- src/main/java/com/acme/cards/command/Card.java
+++ src/main/java/com/acme/cards/command/Card.java
@@ -21,24 +21,4 @@ public class Card {
     @Column
     private CardStatus cardStatus;
 
-    public Card() {
-    }
-
-    public Card(UUID cardId, BigDecimal availableBalance, CardStatus cardStatus) {
-        this.cardId = cardId;
-        this.availableBalance = availableBalance;
-        this.cardStatus = cardStatus;
-    }
-
-    public boolean canAuthorize(BigDecimal amount) {
-        return cardStatus == CardStatus.ACTIVE && availableBalance.compareTo(amount) >= 0;
-    }
-
-    public void debit(BigDecimal amount) {
-        this.availableBalance = this.availableBalance.subtract(amount);
-    }
-
-    public UUID getCardId() { return cardId; }
-    public BigDecimal getAvailableBalance() { return availableBalance; }
-    public CardStatus getStatus() { return cardStatus; }
 }

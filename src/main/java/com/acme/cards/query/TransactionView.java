dea1b81fe72a230bc17a7519cf66e6575ad56e62
Otavio Santana <otaviopolianasantana@gmail.com>
2026-04-29 18:56:54 +0100

    refactor: convert TransactionView class to record and remove redundant methods

--- src/main/java/com/acme/cards/query/TransactionView.java
+++ src/main/java/com/acme/cards/query/TransactionView.java
@@ -6,60 +6,23 @@ import jakarta.nosql.Id;
 
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.util.Objects;
 import java.util.UUID;
 
 @Entity
-public class TransactionView {
+public record TransactionView (@Id
+                               UUID id,
 
-    @Id
-    private UUID id;
+                                       @Column
+                                       UUID cardId,
 
-    @Column
-    private UUID cardId;
+                                       @Column
+                                       BigDecimal amount,
 
-    @Column
-    private BigDecimal amount;
+                                       @Column
+                                       String status,
 
-    @Column
-    private String status;
-
-    @Column
-    private Instant createdAt;
+                                       @Column
+                                       Instant createdAt) {
 
 
-    public TransactionView(UUID id, UUID cardId, BigDecimal amount, String status, Instant createdAt) {
-        this.id = id;
-        this.cardId = cardId;
-        this.amount = amount;
-        this.status = status;
-        this.createdAt = createdAt;
-    }
-
-    public TransactionView() {
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof TransactionView that)) {
-            return false;
-        }
-        return Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
-
-    @Override
-    public String toString() {
-        return "TransactionView{" +
-                "id=" + id +
-                ", cardId=" + cardId +
-                ", amount=" + amount +
-                ", status='" + status + '\'' +
-                ", createdAt=" + createdAt +
-                '}';
-    }
 }

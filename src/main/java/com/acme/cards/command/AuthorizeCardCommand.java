0807e4f5fbfba35c0d1fec072cfb89f5af741202
Otavio Santana <otaviopolianasantana@gmail.com>
2026-04-29 18:00:36 +0100

    refactor: simplify AuthorizeCardCommand record declaration

--- src/main/java/com/acme/cards/command/AuthorizeCardCommand.java
+++ src/main/java/com/acme/cards/command/AuthorizeCardCommand.java
@@ -3,8 +3,6 @@ package com.acme.cards.command;
 import java.math.BigDecimal;
 import java.util.UUID;
 
-public record AuthorizeCardCommand(
-        UUID cardId,
-        BigDecimal amount
-) {
+public record AuthorizeCardCommand(UUID cardId, BigDecimal amount) {
+
 }
